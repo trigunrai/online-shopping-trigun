0ba0c0f8b30055bd9d183bf229664a8b7d6d15ee
Trigun Rai <trigunrai15@gmail.com>
2020-02-13 14:59:49 +0530

    My Project Setup Completed Successfully

---
+++ shoppingbackend/src/main/java/com/zensar/trigun/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.zensar.trigun.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
